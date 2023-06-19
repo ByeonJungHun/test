435e496a2c3f3f9198ff2291efd0058adf80e930
ByeonJungHun <131643404+ByeonJungHun@users.noreply.github.com>
2023-06-19 10:45:22 +0900

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export USER="ByeonJungHun"
-export GITKEY="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"
+#export GITKEY="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"
 export REPO=$1
 
 echo ${REPO/\/\//\/\/$USER:$GITKEY@}
