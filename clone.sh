fece2f71e2f4914515a4cc0ddb25970724cbca35
ByeonJungHun <131643404+ByeonJungHun@users.noreply.github.com>
2023-06-19 11:28:30 +0900

    1234

--- clone.sh
+++ clone.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export USER="ByeonJungHun"
-#export GITKEY="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"
+export GITKEY="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"
 add test
 export REPO=$1
 
